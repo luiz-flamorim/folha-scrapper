804b1ce8206afd79b6c7ff69bcececb32d9570ef
luiz-flamorim <luiz.flamorim@gmail.com>
2023-08-07 23:50:02 +0100

    fixing the packs

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({ origin: 'https://www.herokucdn.com', credentials: true }));
+app.use(cors({ origin: 'https://folha-scrapper-117789299b68.herokuapp.com/', credentials: true }));
 
 app.get('/', async (req, res) => {
   try {
