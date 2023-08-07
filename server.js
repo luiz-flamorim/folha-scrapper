1987550bbc289b031afffe63ff06a7d7986ca368
luiz-flamorim <luiz.flamorim@gmail.com>
2023-08-07 22:20:21 +0100

    trying the heroku deployment

--- server.js
+++ server.js
@@ -19,22 +19,43 @@
 // });
 
 
-
 const express = require('express');
-const { collectH2Elements } = require('./scrapper.js'); // Adjust the path as needed
+const { collectH2Elements } = require('./scraper'); // Import the scraper function
 
 const app = express();
 
 app.get('/', async (req, res) => {
   try {
-    // Call your scraper function from scrapper.js
+    // Call the scraper function directly
     const scrapingResult = await collectH2Elements();
     
-    // Process the scrapingResult and save it to a JSON file if needed
+    // Format the scraping result as HTML elements
+    const resultHTML = scrapingResult.map((item, index) => `
+      <div>
+        <h2>${item.Title}</h2>
+        <p><strong>Link:</strong> <a href="${item.Link}" target="_blank">${item.Link}</a></p>
+        <p><strong>Overview:</strong> ${item.Overview}</p>
+      </div>
+    `).join('');
+    
+    // Create the complete HTML response
+    const htmlResponse = `
+      <html>
+        <head>
+          <title>Scraping Results</title>
+        </head>
+        <body>
+          <h1>Scraping Completed</h1>
+          <div>
+            ${resultHTML}
+          </div>
+        </body>
+      </html>
+    `;
     
     res
       .status(200)
-      .send('Scraping completed and results saved')
+      .send(htmlResponse)
       .end();
   } catch (error) {
     console.error('Error during scraping:', error);
